5f9179fc3a0d5ee822a3155a8789fa704e4e9419
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-04-06 13:06:20 +0200

    p2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="background-image: url('/img/Cover2.jpg');background-size: cover;">
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
